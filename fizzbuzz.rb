e33cf0c122702b3d15b2dbc6d1e8bb70e45ac68d
danl411412 <daan.b.lee@gmail.com>
2018-01-22 22:45:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,4 @@
 def fizzbuzz(num)
-  fizz = num % 3 == 0
-  buzz = num % 5 == 0
-  fizzbuzz = fizz && buzz
-  if fizzbuzz
-    return "FizzBuzz"
-  elsif fizz
-    return "Fizz"
-  elsif buzz
-    return "Buzz"
-  end
+
 
 end
